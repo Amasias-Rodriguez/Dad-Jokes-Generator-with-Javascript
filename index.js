a6c01ea2c2e96f039cb90c2367cabf7240ab8dd7
Amasías Rodríguez <100006082+Amasias-Rodriguez@users.noreply.github.com>
2023-04-01 21:06:02 -0600

    Update index.js

--- index.js
+++ index.js
@@ -13,16 +13,23 @@ const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1"
 
 async function getJoke(){
 
-    jokeEl.innerText = "Updating...";
-    btnEl.disable = true;
-    btnEl.innerText = "Loading...";
-    const response = await fetch(apiURL, options)
-    const data = await response.json();
-
-    btnEl.disable = true;
-    btnEl.innerText = "Tell me a Joke";
-
-    jokeEl.innerText = data[0].joke;
+    try{
+        jokeEl.innerText = "Updating...";
+        btnEl.disable = true;
+        btnEl.innerText = "Loading...";
+        const response = await fetch(apiURL, options)
+        const data = await response.json();
+    
+        btnEl.disable = false;
+        btnEl.innerText = "Tell me a Joke";
+    
+        jokeEl.innerText = data[0].joke;
+    } catch (error) {
+        jokeEl.innerText = "An error happened, try again later";
+        btnEl.disable = false;
+        btnEl.innerText = "Tell me a Joke";
+       console.log(error); 
+    }
 }
 
-btnEl.addEventListener("click", getJoke)
+btnEl.addEventListener("click", getJoke);
